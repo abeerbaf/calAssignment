48540f155ded68effe6ba9279b2eaf042566e328
abeerbaf <abeer.bafagih@starthmore.edu>
2022-06-24 22:06:42 +0300

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
-// *** Write your code here***
+
+
+let display = document.querySelector('#display')
+
+function calcNumbers(x){
+    display.value = display.value + x;
+    console.log(x);
+}
+
+function specKeys(y){
+    console.log(y)
+    if(y === 'c'){
+        display.value="";
+    }else if(y === 'del'){
+        let arr =display.value.split('');
+       arr.pop();
+       display.value = arr.join('')
+    }
+}
